a071be1ef74d0e77d9c72f146a38d85ca953c7a2
Xander T <xanderthomp@gmail.com>
2025-10-13 14:08:49 -0700

    Quartz sync: Oct 13, 2025, 2:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "www.deltrese.com",
-    ignorePatterns: ["private", "templates", ".obsidian", ".trash"],
+    ignorePatterns: ["private", "templates", ".trash"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
