7d895890facbbeb16810371a13516fc0d68a2d5d
Xander T <xanderthomp@gmail.com>
2025-10-20 11:06:09 -0700

    Quartz sync: Oct 20, 2025, 11:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,9 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
+      Plugin.TableOfContents({
+        maxDepth: 4,
+      }),
       Plugin.CrawlLinks({
          markdownLinkResolution: "shortest",
          openLinksInNewTab: true,
